3b64017413509da06104b5513adddcc6ea47a1f2
asish012 <asish012@gmail.com>
2012-06-15 18:27:22 +0300

    goal: to separated the allTask page from the main.html page. a new page has been added for allTask(allTask.html). other changes are to accomplish the current need.

--- Heinzelmaennchen/app/controllers/Application.java
+++ Heinzelmaennchen/app/controllers/Application.java
@@ -2,13 +2,21 @@ package controllers;
 
 import play.mvc.Controller;
 
-public class Application extends Controller {
+public class Application extends Controller
+{
 
-    public static void index() {
-        render();
-    }
+	public static void index()
+	{
+		render();
+	}
 
-    public static void postATask() {
-        render();
-    }
+	public static void postATask()
+	{
+		render();
+	}
+
+	public static void allTask()
+	{
+		render();
+	}
 }
