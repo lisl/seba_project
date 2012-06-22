9e3f276c5426e47c7c595b1d9b36b19fa53fcf44
nrainer <nrainer@gmail.com>
2012-06-23 00:55:55 +0200

    added Utilities class

---
+++ app/util/Utilities.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utilities
+{
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd.MM.yyyy");
+	
+	public static String formatDate(Date date)
+	{
+		return DATE_FORMAT.format(date);
+	}
+}
